5eda8cff02982a80e084f5c33a234a2a5dd767a3
emanuele sacchetti <manusacco92@gmail.com>
2023-03-24 14:38:20 +0100

    aggiunto titolo nell'app.vue

---
+++ src/components/appTitle.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>La mia prima App con Vite</h1>
+</template>
+
+
+
+
+<style scoped>
+    h1{
+        text-align: center;
+        font-size: 40px;
+        color: brown;
+    }
+</style>
